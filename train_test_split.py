b40101ede0c10992c13a6e74b1a88ba9a7edb3b9
Aditya <adityanagjsr2003@gmail.com>
2026-04-26 16:45:27 +0530

    Create train_test_split.py
    lesser the mae , more accurate the model

---
+++ train_test_split.py
@@ -0,0 +1,13 @@
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+model1.fit(X_train, y_train)
+
+from sklearn.metrics import mean_absolute_error
+
+preds = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, preds)
+
+print(mae)
