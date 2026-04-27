885b5009e6194cfd421ba8637c6b1015f40b0837
Aditya <adityanagjsr2003@gmail.com>
2026-04-27 08:33:45 +0530

    Update train_test_split.py

--- train_test_split.py
+++ train_test_split.py
@@ -4,10 +4,3 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 model.fit(X_train, y_train)
 
-from sklearn.metrics import mean_absolute_error
-
-preds = model.predict(X_test)
-
-mae = mean_absolute_error(y_test, preds)
-
-print(mae)
